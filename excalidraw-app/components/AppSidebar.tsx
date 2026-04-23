8ce4f85ce9cb4c42e02e5a16d785268cefff3aca
Habib Bukhari <habibbukhari@habibs-mbp.mynetworksettings.com>
2026-04-23 15:47:41 -0400

    Fix duplicate presentation sidebar tab

--- excalidraw-app/components/AppSidebar.tsx
+++ excalidraw-app/components/AppSidebar.tsx
@@ -1,8 +1,5 @@
 import { DefaultSidebar, Sidebar, THEME } from "@excalidraw/excalidraw";
-import {
-  messageCircleIcon,
-  presentationIcon,
-} from "@excalidraw/excalidraw/components/icons";
+import { messageCircleIcon } from "@excalidraw/excalidraw/components/icons";
 import { LinkButton } from "@excalidraw/excalidraw/components/LinkButton";
 import { useUIAppState } from "@excalidraw/excalidraw/context/ui-appState";
 
@@ -19,12 +16,6 @@ export const AppSidebar = () => {
           style={{ opacity: openSidebar?.tab === "comments" ? 1 : 0.4 }}
         >
           {messageCircleIcon}
-        </Sidebar.TabTrigger>
-        <Sidebar.TabTrigger
-          tab="presentation"
-          style={{ opacity: openSidebar?.tab === "presentation" ? 1 : 0.4 }}
-        >
-          {presentationIcon}
         </Sidebar.TabTrigger>
       </DefaultSidebar.TabTriggers>
       <Sidebar.Tab tab="comments">
@@ -45,30 +36,6 @@ export const AppSidebar = () => {
             href={`${
               import.meta.env.VITE_APP_PLUS_LP
             }/plus?utm_source=excalidraw&utm_medium=app&utm_content=comments_promo#excalidraw-redirect`}
-          >
-            Sign up now
-          </LinkButton>
-        </div>
-      </Sidebar.Tab>
-      <Sidebar.Tab tab="presentation" className="px-3">
-        <div className="app-sidebar-promo-container">
-          <div
-            className="app-sidebar-promo-image"
-            style={{
-              ["--image-source" as any]: `url(/oss_promo_presentations_${
-                theme === THEME.DARK ? "dark" : "light"
-              }.svg)`,
-              backgroundSize: "60%",
-              opacity: 0.4,
-            }}
-          />
-          <div className="app-sidebar-promo-text">
-            Create presentations with Excalidraw+
-          </div>
-          <LinkButton
-            href={`${
-              import.meta.env.VITE_APP_PLUS_LP
-            }/plus?utm_source=excalidraw&utm_medium=app&utm_content=presentations_promo#excalidraw-redirect`}
           >
             Sign up now
           </LinkButton>
